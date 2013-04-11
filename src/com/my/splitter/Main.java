b26a77a9064c0ecdcd5cc167a55b35d63f106377
Mike <gru_mike@inbox.ru>
2013-04-12 00:31:34 +0400

    Add file dialog to Browse button handler

--- src/com/my/splitter/Main.java
+++ src/com/my/splitter/Main.java
@@ -2,21 +2,22 @@ package com.my.splitter;
 
 import java.awt.Container;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-import javax.swing.JFrame;
-import java.awt.GridLayout;
-
-import javax.swing.Box;
-import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.BoxLayout;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import java.awt.Window.Type;
+import javax.swing.JTextField;
 
 public class Main {
 
 	private JFrame frame;
 	private JTextField textField;
+	private final JFileChooser fc = new JFileChooser();
 
 	/**
 	 * Launch the application.
@@ -45,23 +46,35 @@ public class Main {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
 		frame = new JFrame();
 		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
+		Container contentPane = frame.getContentPane();
+		contentPane.setLayout(null);
+
 		JPanel panel = new JPanel();
 		panel.setBounds(43, 90, 10, 10);
-		frame.getContentPane().add(panel);
-		
+		contentPane.add(panel);
+
 		textField = new JTextField();
 		textField.setBounds(10, 11, 318, 20);
-		frame.getContentPane().add(textField);
+		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnBrowse = new JButton("Browse");
+		btnBrowse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int result = fc.showDialog(frame, "Pick up the file to split");
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					JOptionPane.showMessageDialog(frame,
+							"File: " + file.toPath());
+				}
+			}
+		});
 		btnBrowse.setBounds(338, 10, 89, 23);
-		frame.getContentPane().add(btnBrowse);
+		contentPane.add(btnBrowse);
 	}
 }
